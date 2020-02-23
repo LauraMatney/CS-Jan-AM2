12a5d5887f4f37fbc255321caa9226e7eca6ee1a
LauraMatney <laura.matney@indwes.edu>
2020-02-23 08:11:36 -0500

    Added Unity Project

---
+++ Assets/Scripts/Paintball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintball : MonoBehaviour
+{
+    public List<Material> ourPaints = new List<Material>();
+    static private int paintIndex = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Paintable")
+        {
+            collision.collider.GetComponent<Renderer>().material = ourPaints[paintIndex];
+            paintIndex++;
+            if(paintIndex == ourPaints.Count)
+            {
+                paintIndex = 0;
+            }
+        }
+    }
+}
